a9d61bbbe95783f346376c1605add31d610ae847
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-08-28 15:43:05 +0200

    Give-up-commit

---
+++ Assets/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+
+	GameObject bg;
+
+	void Start ()
+	{
+		bg = GameObject.Find ("bg");
+		bg.transform.position = new Vector2 (0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		print (Camera.main.orthographicSize);
+		print (Camera.main.aspect);
+	}
+}
